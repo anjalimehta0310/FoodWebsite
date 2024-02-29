4c9fb051a34ebcac8697f2a87d2cf08f691ee846
Anjali Mehta <121865600+anjalimehta0310@users.noreply.github.com>
2024-02-29 10:32:48 +0530

    Update useRestarurant.js

--- src/Utilis/useRestarurant.js
+++ src/Utilis/useRestarurant.js
@@ -8,7 +8,7 @@ const UseRestarurant=(resId)=>{
         const data=await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.7022686&lng=77.4192428&restaurantId="+resId+"&catalog_qa=undefined&submitAction=ENTER");
         const json=await data.json();
         console.log(json);
-        setRestaurant(json.data?.cards[2]?.card?.card?.info);
+        setRestaurant(json.data?.cards[0]?.card?.card?.info);
     }
     return restaurant;
 };
